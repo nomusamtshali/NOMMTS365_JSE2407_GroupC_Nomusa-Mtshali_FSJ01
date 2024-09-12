47261f561e435a7d4ebae2775c4eb624849c6666
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-09-12 16:18:18 +0200

    updated Footer component with about section and social media links and added react-icons dependency

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,11 +1,42 @@
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 p-4 mt-4">
-      <div className="container mx-auto text-center text-white">
-        &copy; 2024 | Nomusa Mtshali | The Fashionista's Closet 
+    <footer className="bg-white shadow-lg text-black py-2">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 text-center">
+        
+        {/* About Section */}
+        <div>
+          <h3 className="text-xl font-bold mb-4">MaxiMart</h3>
+          <p className="text-md font-semibold">
+          Everything Under One Roof!
+          </p>
+        </div>
+
+        {/* Social Media Links */}
+        <div>
+          <h3 className="text-xl font-bold mb-4">Follow Us</h3>
+          <div className="flex justify-center space-x-4">
+            <a href="#" className="hover:text-gray-400">
+              <FaFacebook size={20} />
+            </a>
+            <a href="#" className="hover:text-gray-400">
+              <FaInstagram size={20} />
+            </a>
+            <a href="#" className="hover:text-gray-400">
+              <FaTwitter size={20} />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* Copyright Section */}
+      <div className="mt-4 mb-2 font-bold text-center text-md">
+        &copy; 2024 | Nomusa Mtshali | MaxiMart
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
+
