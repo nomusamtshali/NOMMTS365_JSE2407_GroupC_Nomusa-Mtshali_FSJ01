382e21788fd829a9d40de63235b2f086e4aa2c8a
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-09-12 16:17:30 +0200

    updated Header component with new design (navigation links) and added react-icons dependency

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,11 +1,37 @@
+import { FaShoppingCart, FaHeart, FaUser } from "react-icons/fa";
+
 const Header = () => {
   return (
-   <nav className="bg-white p-4">
-    <div className="container mx-auto">
-      <h1 className="text-black text-xl">The Fashionista's Closet</h1>
-    </div>
-  </nav>
-  )
-}
+    <nav className="bg-white shadow-lg p-6">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <h1 className="text-black text-2xl font-bold">MaxiMart</h1>
 
-export default Header
+        {/* Navigation Links */}
+        <div className="flex space-x-6">
+          
+          {/* Cart */}
+          <div className="flex items-center space-x-2 cursor-pointer hover:text-gray-600">
+            <FaShoppingCart className="text-lg" />
+            <span className="text-md font-medium">Cart</span>
+          </div>
+
+          {/* Wishlist */}
+          <div className="flex items-center space-x-2 cursor-pointer hover:text-gray-600">
+            <FaHeart className="text-lg" />
+            <span className="text-md font-medium">Wishlist</span>
+          </div>
+
+          {/* Account */}
+          <div className="flex items-center space-x-2 cursor-pointer hover:text-gray-600">
+            <FaUser className="text-lg" />
+            <span className="text-md font-medium">Account</span>
+          </div>
+
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
